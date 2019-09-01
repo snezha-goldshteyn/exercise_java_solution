6abbbb57c06151ce39b7841dcfd0915ba1084cba
vnutri-snega <snezha.goldshteyn@gmail.com>
2019-09-01 07:48:42 +0300

    Fixed InputValidator exceptions

--- src/main/java/com/exercise/quotes/validation/InputValidator.java
+++ src/main/java/com/exercise/quotes/validation/InputValidator.java
@@ -1,17 +1,18 @@
 package com.exercise.quotes.validation;
 
 import com.exercise.quotes.dto.QuoteDto;
+import com.exercise.quotes.exceptions.QuoteException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class InputValidator {
     public static void validate(QuoteDto quoteDto) {
         if (quoteDto.getName().equals("")) {
-            throw new IllegalStateException("Name cannot be empty");
+            throw new QuoteException("Name cannot be empty", 4);
         }
 
         if (quoteDto.getPrice()<0) {
-            throw new IllegalStateException("Price cannot be negative");
+            throw new QuoteException("Price cannot be negative", 4);
         }
     }
 }
